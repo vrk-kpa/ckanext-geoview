641891e280b48a497b4e2b7aa1bc4a6c22d18cfb
Sol Lee <u103133.u103135@gmail.com>
2024-02-05 06:56:09 +0000

    Add leaflet-providers to webassets and attributionControl to JS modules

--- ckanext/geoview/public/js/shp_preview.js
+++ ckanext/geoview/public/js/shp_preview.js
@@ -19,7 +19,7 @@ ckan.module('shppreview', function (jQuery, _) {
 
       self.el.empty();
       self.el.append($('<div></div>').attr('id', 'map'));
-      self.map = ckan.commonLeafletMap('map', this.options.map_config);
+      self.map = ckan.commonLeafletMap('map', this.options.map_config, {attributionControl: false});
 
       // hack to make leaflet use a particular location to look for images
       L.Icon.Default.imagePath = this.options.site_url + 'js/vendor/leaflet/dist/images';
